366669cff09c0e5480f3e6377157f15dbbf74f34
lshlyapnikov <lshlyapnikov@gmail.com>
2018-11-02 05:29:22 -0400

    Id monad

--- exercises/src/main/scala/fpinscala/monads/Monad.scala
+++ exercises/src/main/scala/fpinscala/monads/Monad.scala
@@ -138,14 +138,18 @@ object Monad {
 
   def stateMonad[S] = ???
 
-  lazy val idMonad: Monad[Id] = ???
+  lazy val idMonad: Monad[Id] = new Monad[Id] {
+    override def unit[A](a: => A): Id[A] = Id(a)
+
+    override def flatMap[A, B](ma: Id[A])(f: A => Id[B]): Id[B] = ma.flatMap(f)
+  }
 
   def readerMonad[R] = ???
 }
 
 case class Id[A](value: A) {
-  def map[B](f: A => B): Id[B]         = ???
-  def flatMap[B](f: A => Id[B]): Id[B] = ???
+  def map[B](f: A => B): Id[B]         = Id(f(value))
+  def flatMap[B](f: A => Id[B]): Id[B] = f(value)
 }
 
 object Reader {
